837ae35edd115a136973f870297e1ad6ffc2b49d
khanbeb <s.munnakhan@gmail.com>
2020-08-11 02:14:05 +0600

    new

---
+++ template.js
@@ -0,0 +1,12 @@
+const firstName = "Sajal";
+const LastName = "Khan";
+const fullName = firstName + " "+ LastName + " is a good boy";
+const fullName2 = `${firstName} ${LastName} is a good boy`
+console.log(fullName2);
+
+
+const multiLine = `I love you
+i miss you
+i need you`
+
+console.log(multiLine);
